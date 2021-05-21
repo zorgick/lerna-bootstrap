d5485e58b1cc8c3ae80d33aee06343c24b0b75f9
Zackary Kononov <zorgizorgik@gmail.com>
2021-05-21 21:38:16 +0300

    fix(storybook): make storybook work with emotion

--- modules/components/.storybook/main.js
+++ modules/components/.storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-css-modules-preset",
-  ]
+  ],
+  babel: config => {
+    config.presets[2][1] = { runtime: 'classic' } // make emotion work with storybook
+    return config
+  },
 }
